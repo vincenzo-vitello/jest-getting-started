57922f1bd819d612acb6d31e3d7c076bf6e43f84
Vincenzo Vitello <vincenzo.vitello@icloud.com>
2025-02-27 12:05:42 +0100

    tested more jest functions

--- src/sum.test.js
+++ src/sum.test.js
@@ -7,13 +7,22 @@ test('makes 1 + 1', () => {
 });
 
 test('makes 2 + 2', () => {
-  expect(sum(2, 2)).toBe(4);
+  expect(sum(2, 2)).toBeTruthy();
 });
 
 test('makes -1 + 1', () => {
-  expect(sum(-1, 1)).toBe(0);
+  expect(sum(-1, 1)).toBeFalsy();
 });
 
 test('makes 0 + 0', () => {
-  expect(sum(0, 0)).toBe(0);
+  expect(sum(0, 0)).toEqual(0);
+});
+
+test('makes 0 + 0', () => {
+  expect(sum(0, 0)).not.toBeNull();
+});
+
+test('makes 1 + 2', () => {
+  expect(sum(1, 2)).toBeGreaterThan(2);
+  expect(sum(1, 2)).toBeLessThan(5);
 });
